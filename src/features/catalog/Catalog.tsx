17fe74396657a92e3949ba3fa9dcf854c3a99342
Đặng Thanh Tuấn <91366633+ketuan1@users.noreply.github.com>
2023-01-10 11:07:20 +0700

    upload code client reactjs

---
+++ src/features/catalog/Catalog.tsx
@@ -0,0 +1,26 @@
+import { Product } from "../../model/product";
+
+interface Props {
+  products: Product[],
+  onAddProduct: () => void;
+}
+
+
+function Catalog(props: Props) {
+  return (
+    <>
+      <button onClick={props.onAddProduct}>Add Products</button>
+      <ul>
+        {props.products.map((product: any, index: any) => {
+          return (
+            <li key={index}>
+              {product.name} - price: {product.unitPrice}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default Catalog;
---
+++ src/layout/App.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Product } from "../model/product";
+import Catalog from "../features/catalog/Catalog";
+
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/products")
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  //function add Products
+  const addProduct = () => {
+    setProducts((prevState) => [
+      ...prevState,
+      {
+        id: prevState.length + 1,
+        name: "product" + (prevState.length + 1),
+        description: "desc product" + (prevState.length + 1),
+        unitPrice: 11.11 * (prevState.length + 1),
+        imageUrl: 'NONE',
+        brand: 'NA',
+        unitsInStock: 100,
+        category: 'unknown'
+      }
+    ]);
+    console.log(products);
+  };
+
+  return (
+    <div className="app">
+      <h1>My Shops</h1>
+      <Catalog products={products} onAddProduct={addProduct} />
+    </div>
+  );
+}
+
+export default App;
